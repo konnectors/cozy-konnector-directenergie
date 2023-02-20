d779abb0e86b1102fb11d5cbf559224070791af6
Killian Courvoisier <killian.courvoisier@cozycloud.cc>
2023-02-20 11:30:54 +0100

    Migrate from CCC template

--- src/index.js
+++ src/index.js
@@ -1,231 +1,86 @@
-process.env.SENTRY_DSN =
-  process.env.SENTRY_DSN ||
-  'https://0e2e4e682d4d40fcade8082a84537fed@errors.cozycloud.cc/10'
+import { ContentScript } from 'cozy-clisk/dist/contentscript'
+import Minilog from '@cozy/minilog'
+const log = Minilog('ContentScript')
+Minilog.enable()
 
-const {
-  BaseKonnector,
-  requestFactory,
-  scrape,
-  log,
-  utils,
-  signin,
-  cozyClient
-} = require('cozy-konnector-libs')
+const baseUrl = 'http://toscrape.com'
+const defaultSelector = "a[href='http://quotes.toscrape.com']"
+const loginLinkSelector = `[href='/login']`
+const logoutLinkSelector = `[href='/logout']`
 
-const moment = require('moment')
-
-const models = cozyClient.new.models
-const { Qualification } = models.document
-
-const request = requestFactory({
-  // The debug mode shows all the details about HTTP requests and responses. Very useful for
-  // debugging but very verbose. This is why it is commented out by default
-  // debug: true,
-  // Activates [cheerio](https://cheerio.js.org/) parsing on each page
-  cheerio: true,
-  // If cheerio is activated do not forget to deactivate json parsing (which is activated by
-  // default in cozy-konnector-libs
-  json: false,
-  // This allows request-promise to keep cookies between requests
-  jar: true
-})
-
-const baseUrl = 'https://www.totalenergies.fr'
-
-const courl = baseUrl + '/clients/connexion'
-
-module.exports = new BaseKonnector(start)
-
-// The start function is run by the BaseKonnector instance only when it got all the account
-// information (fields). When you run this connector yourself in "standalone" mode or "dev" mode,
-// the account information come from ./konnector-dev-config.json file
-// cozyParameters are static parameters, independents from the account. Most often, it can be a
-// secret api key.
-async function start(fields, cozyParameters) {
-  log('info', 'Identification')
-  if (cozyParameters) log('debug', 'Paramètres trouvés')
-  await authenticate.bind(this)(fields.login, fields.password)
-  log('info', 'Vous êtes connecté')
-  const bills = await parseBill()
-
-  await this.saveBills(bills, fields, {
-    fileIdAttributes: ['vendorRef'],
-    linkBankOperations: false,
-    identifiers: ['Total energie'],
-    sourceAccount: this.accountId,
-    sourceAccountIdentifier: fields.login
-  })
-  log('info', 'Fin de la récupération')
-}
-
-async function authenticate(username, password) {
-  log('debug', 'Authentification en cours')
-  await signin({
-    url: courl,
-    formSelector: '#fz-authentificationForm',
-    formData: {
-      'tx_demmauth_authentification[authentificationForm][login]': username,
-      'tx_demmauth_authentification[authentificationForm][password]': password
-    },
-    resolveWithFullResponse: true
-  })
-    .catch(err => {
-      log('err', err)
-    })
-    .then(resp => {
-      return resp
-    })
-}
-
-async function parseBill() {
-  log('debug', 'Vérification des factures')
-  let $
-  try {
-    $ = await request(
-      `https://www.totalenergies.fr/clients/mes-factures/mes-factures-electricite/mon-historique-de-factures`
-    )
-  } catch (err) {
-    log('debug', err.message.substring(0, 60))
-    log('debug', `Pas de facture trouvée pour ce compte`)
-    return []
+class TemplateContentScript extends ContentScript {
+  async ensureAuthenticated() {
+    await this.goto(baseUrl)
+    await this.waitForElementInWorker(defaultSelector)
+    await this.runInWorker('click', defaultSelector)
+    // wait for both logout or login link to be sure to check authentication when ready
+    await Promise.race([
+      this.waitForElementInWorker(loginLinkSelector),
+      this.waitForElementInWorker(logoutLinkSelector)
+    ])
+    const authenticated = await this.runInWorker('checkAuthenticated')
+    if (!authenticated) {
+      this.log('info', 'Not authenticated')
+      await this.showLoginFormAndWaitForAuthentication()
+    }
+    return true
   }
 
-  const docs = scrape(
-    $,
-    {
-      label: {
-        sel: '.detail-facture__label strong'
-      },
-      vendorRef: {
-        sel: '.text--body',
-        parse: ref => ref.match(/^N° (.*)$/).pop()
-      },
-      date: {
-        sel: '.detail-facture__date',
-        parse: date => moment(date, 'DD/MM/YYYY').toDate()
-      },
-      status: {
-        sel: '.detail-facture__statut'
-      },
-      amount: {
-        sel: '.detail-facture__montant',
-        parse: normalizeAmount
-      },
-      isEcheancier: {
-        sel: '.detail-facture__action.btn-bas-nivo2',
-        attr: 'class',
-        parse: Boolean
-      },
-      fileurl: {
-        sel: '.btn--telecharger',
-        attr: 'href'
-      },
-      subBills: {
-        sel: 'span:nth-child(1)',
-        fn: el => {
-          const $details = $(el)
-            .closest('.detail-facture')
-            .next()
+  async checkAuthenticated() {
+    return Boolean(document.querySelector(logoutLinkSelector))
+  }
 
-          if ($details.hasClass('action__display-zone')) {
-            const fileurl = $details.find('.btn--telecharger').attr('href')
-            return Array.from($details.find('tbody tr'))
-              .map(el => {
-                let date = $(el)
-                  .find('td:nth-child(4)')
-                  .text()
-                  .match(/Payée le (.*)/)
-                if (date) date = moment(date.slice(1), 'DD/MM/YYYY').toDate()
-                return {
-                  amount: normalizeAmount(
-                    $(el)
-                      .find('td:nth-child(2)')
-                      .text()
-                  ),
-                  date,
-                  fileurl
-                }
-              })
-              .filter(bill => bill.date)
-          }
+  async showLoginFormAndWaitForAuthentication() {
+    log.debug('showLoginFormAndWaitForAuthentication start')
+    await this.clickAndWait(loginLinkSelector, '#username')
+    await this.setWorkerState({ visible: true })
+    await this.runInWorkerUntilTrue({
+      method: 'waitForAuthenticated'
+    })
+    await this.setWorkerState({ visible: false })
+  }
 
-          return false
-        }
-      }
-    },
-    '.detail-facture'
-  ).filter(bill => !(bill.amount === false && bill.isEcheancier === false))
+  async fetch(context) {
+    log.debug(context, 'fetch context')
+    const bookLinkSelector = `[href*='books.toscrape.com']`
+    await this.goto(baseUrl + '/index.html')
+    await this.waitForElementInWorker(bookLinkSelector)
+    await this.clickAndWait(bookLinkSelector, '#promotions')
+    const bills = await this.runInWorker('parseBills')
 
-  const bills = []
-
-  for (const doc of docs) {
-    if (doc.subBills) {
-      for (const subBill of doc.subBills) {
-        const { vendorRef, label } = doc
-        const echDate = doc.date
-        const { amount, date, fileurl } = subBill
-        bills.push({
-          vendorRef,
-          label,
-          amount,
-          date,
-          fileurl: `https://www.totalenergies.fr${fileurl}`,
-          filename: `${utils.formatDate(echDate)}_TotalEnergies_Echéancier.pdf`,
-          vendor: 'Direct Energie',
-          fileAttributes: {
-            metadata: {
-              contentAuthor: 'totalenergies.fr',
-              issueDate: utils.formatDate(date),
-              datetime: utils.formatDate(date),
-              datetimeLabel: `issueDate`,
-              invoiceNumber: `${vendorRef}`,
-              isSubscription: true,
-              carbonCopy: true,
-              qualification: Qualification.getByLabel('energy_invoice')
-            }
-          }
-        })
-      }
-    } else {
-      const { vendorRef, label, date, fileurl, amount, status } = doc
-      const isRefund = status.includes('Remboursée')
-      bills.push({
-        vendorRef,
-        label,
-        amount,
-        date,
-        isRefund,
-        fileurl: `https://www.totalenergies.fr${fileurl}`,
-        filename: `${utils.formatDate(date)}_TotalEnergies_${amount.toFixed(
-          2
-        )}EUR_${vendorRef}.pdf`,
-        fileIdAttributes: ['vendorRef'],
-        vendor: 'Direct Energie',
-        fileAttributes: {
-          metadata: {
-            contentAuthor: 'totalenergies.fr',
-            issueDate: utils.formatDate(date),
-            datetime: utils.formatDate(date),
-            datetimeLabel: `issueDate`,
-            invoiceNumber: `${vendorRef}`,
-            isSubscription: true,
-            carbonCopy: true,
-            qualification: Qualification.getByLabel('energy_invoice')
-          }
-        }
+    for (const bill of bills) {
+      await this.saveFiles([bill], {
+        contentType: 'image/jpeg',
+        fileIdAttributes: ['filename'],
+        context
       })
     }
   }
-  return bills
+
+  async getUserDataFromWebsite() {
+    return {
+      sourceAccountIdentifier: 'defaultTemplateSourceAccountIdentifier'
+    }
+  }
+
+  async parseBills() {
+    const articles = document.querySelectorAll('article')
+    return Array.from(articles).map(article => ({
+      amount: normalizePrice(article.querySelector('.price_color')?.innerHTML),
+      filename: article.querySelector('h3 a')?.getAttribute('title'),
+      fileurl:
+        'https://books.toscrape.com/' +
+        article.querySelector('img')?.getAttribute('src')
+    }))
+  }
 }
 
-const normalizeAmount = amount => {
-  if (amount.includes('/')) return false
-  return parseFloat(
-    amount
-      .replace('€', '')
-      .replace(',', '.')
-      .replace(' ', '')
-      .trim()
-  )
+// Convert a price string to a float
+function normalizePrice(price) {
+  return parseFloat(price.replace('£', '').trim())
 }
+
+const connector = new TemplateContentScript()
+connector.init({ additionalExposedMethodsNames: ['parseBills'] }).catch(err => {
+  log.warn(err)
+})
